749601f052f62e11f970b34b696ed0d9b645b40c
coool8192-arch <coool8192@gmail.com>
2026-02-11 21:42:55 +0900

    260211 클래스

---
+++ Class1st (Stream)/source.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+
+int packet = 1;
+
+void swap(int& parameterX, int& parameterY)
+{
+	int temp = parameterX;
+	parameterX = parameterY;
+	parameterY = temp;
+}
+
+int main()
+{
+#pragma region 스트림
+	// 시간의 흐름에 따라 연속적으로 발생하는 데이터의 흐름입니다.
+
+	//std::cout << "c++";
+	//
+	//int count = 0;
+	//
+	//std::cin >> count;
+	//
+	//for (int i = 0; i < count; i++)
+	//{
+	//	std::cout << "PlayStation "<< i + 1 << std::endl;
+	//}
+
+	// 스트림은 운영 체제에 의해 생성되며,
+	// 스트림 자체에 임시 메모리 공간인 버퍼가 존재합니다.
+
+#pragma endregion
+
+#pragma region 범위 지정 연산자
+	// 여러 범위에서 사용되는 식별자를 구분하는 용도의 연산자입니다.
+
+	//int packet = 100;
+	//
+	//std::cout << packet << std::endl;
+	//std::cout << ::packet << std::endl;
+
+	// 범위 지정 연산자는 전역 변수와 같은 이름의 지역 변수가 선언되었을 때
+	// 가장 가까운 범위에 선언된 변수의 이름을 사용하는 범위 규칙이 있어서 전역 변수를 호출하지 않습니다.
+
+#pragma endregion
+
+#pragma region 참조자
+	// 하나의 변수에 또다른 이름을 지정하는 지정자입니다.
+	//int x = 12;
+	//int y = 20;
+	//
+	//swap(x, y);
+	//
+	//std::cout << "x : " << x << std::endl;
+	//std::cout << "y : " << y << std::endl;
+
+#pragma endregion
+
+#pragma region 동적 할당
+	//int* pointer = new int ;
+	//
+	//*pointer = 10;
+	//
+	//std::cout << "pointer가 가리키는 주소의 값 : " << *pointer << std::endl;
+	//
+	//delete pointer;
+	//
+	//pointer = new int[5];
+	//
+	//pointer[0] = 10;
+	//pointer[1] = 20;
+	//pointer[2] = 30;
+	//pointer[3] = 40;
+	//pointer[4] = 50;
+	//
+	//for (int i = 0; i < 5; i++)
+	//{
+	//std::cout << "pointer["<< i <<"]의 값 : " << pointer[i] << std::endl;
+	//}
+	//
+	//delete[] pointer;
+#pragma endregion
+
+}
---
+++ Class2nd (Class)/source.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+
+using namespace std;
+
+namespace Argentina
+{
+	float area = 2780000;
+	void information()
+	{
+		cout << "수도 부에노스아이레스, 인구 4570만명, 면적" << area << "km²" << endl;
+	}
+}
+
+namespace Brazil
+{
+	float area = 8516000;
+	void information()
+	{
+		cout << "수도 브라질리아, 인구 2.12억명, 면적" << area << "km²" << endl;
+	}
+
+}
+
+using namespace Brazil;
+
+class location
+{
+#pragma region 접근 지정자
+	// 클래스 내부에 포함된 속성에 대한 접근 범위를 제한하는 지정자입니다.
+
+	// public : 클래스 내부, 자기가 상속하는 클래스, 클래스 외부까지 접근을 허용합니다.
+	// protected : 클래스 내부, 자기가 상속하는 클래스까지만 접근을 허용합니다.
+	// private : 클래스 내부까지만 접근을 허용합니다.
+#pragma endregion
+
+	int x = 4;
+	int y = 3;
+	int z = -5;
+
+protected: const char* name = "좌표";
+
+public: void position()
+{
+
+	cout << "현재 위치 : " << x << ", " << y << ", " << z << endl;
+}
+};
+
+void Profile(const char* name = "Jack", int age = 20)
+{
+	cout << "이름 : " << name << endl;
+	cout << "나이 : " << age << endl;
+}
+
+int main()
+{
+#pragma region 이름 공간
+	// 속성을 구분할 수 있도록 유효 범위를 설정하는 영역입니다.
+
+	//Argentina::information();
+	//information();
+#pragma endregion
+
+#pragma region 클래스
+	// 사용자 정의 데이터 유형으로 속성과 함수가 포함되고, 객체를 생성해 접근 및 사용하는 집합체입니다.
+
+	//location location;
+	//
+	//location.position();
+	//
+	//cout << "location 크기 : " << sizeof(location) << endl;
+
+	// 클래스의 경우 내부의 변수는 클래스의 메모리 영역에 포함되지만, 정적 변수와 함수는 포함되지 않습니다.
+#pragma endregion
+
+#pragma region 기본 매개 변수
+	// 함수의 매개 변수에 값이 전달되지 않을 때, 미리 값을 설정해두는 변수입니다.
+
+	Profile();
+	Profile("Kei", 15);
+
+	// 기본 매개 변수는 오른쪽에서부터 값을 지정해주어야 합니다. (컴파일러가 왼쪽부터 순서대로 읽기 때문)
+#pragma endregion
+
+}
+
