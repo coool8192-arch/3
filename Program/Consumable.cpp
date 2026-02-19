c441bdc26805c2ba53e6476f6d976f8f8b861540
coool8192-arch <coool8192@gmail.com>
2026-02-19 21:43:53 +0900

    260219 상속

---
+++ Program/Consumable.cpp
@@ -0,0 +1,14 @@
+#include "Consumable.h"
+#include <iostream>
+
+using namespace std;
+
+Consumable::Consumable()
+{
+	cout << "Consumable Created" << endl;
+}
+
+Consumable::~Consumable()
+{
+	cout << "Consumable Deleted" << endl;
+}
---
+++ Program/Potion.cpp
@@ -0,0 +1,14 @@
+#include "Potion.h"
+#include <iostream>
+
+using namespace std;
+
+Potion::Potion()
+{
+	cout << "Potion Created" << endl;
+}
+
+Potion::~Potion()
+{
+	cout << "Potion Deleted" << endl;
+}
